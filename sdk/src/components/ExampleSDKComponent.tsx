57956c838a7d3ffcec548fe9bcebbf180f3d5a3b
Rui <rzhengphy@gmail.com>
2023-05-21 16:46:28 +0800

    treat zustand same as react to get it work

--- sdk/src/components/ExampleSDKComponent.tsx
+++ sdk/src/components/ExampleSDKComponent.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
+import { create } from "zustand";
+
+const useBearStore = create((set) => ({
+  bears: 13,
+}));
 
 const ExampleSDKComponent = () => {
   useEffect(() => {
     console.log("useEffect called");
   }, []);
 
-  return <div>hello world</div>;
+  const bears = useBearStore((state) => state.bears);
+
+  return <div>hello world {bears} bears</div>;
 };
 
 export default ExampleSDKComponent;
